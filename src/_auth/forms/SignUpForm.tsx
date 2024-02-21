df2802d491a0ba29733b11a8bb148ceaa0f3de53
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-02-21 14:35:00 -0800

    implemet authlayout

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -10,16 +10,19 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from '@/context/AuthContext';
 
 
 const SignUpForm = () => {
   const { toast } = useToast();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
 
-  const  { mutateAsync: createUserAccount, isLoading: isCreatingUser} = useCreateUserAccount();
+  const  { mutateAsync: createUserAccount, isPending: isCreatingAccount} = useCreateUserAccount();
 
-  const  { mutateAsync: signInAccount, isLoading: IsSigningIn } = useSignInAccount();
+  const  { mutateAsync: signInAccount, isPending : IsSigningIn } = useSignInAccount();
 
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -48,7 +51,15 @@ const SignUpForm = () => {
       return toast({ title: "Sign in failed. Please try again"})
     }
 
-    
+    const isLoggedIn = await checkAuthUser();
+
+    if(isLoggedIn) {
+      form.reset();
+
+      navigate('/')
+    } else {
+      return toast({ title: "Sign in failed. Please try again"})
+    }
 
   }
 
@@ -58,7 +69,7 @@ const SignUpForm = () => {
         <img src="/assets/images/logo.svg" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">To use Snapgram, please enter your details</p>
+        <p className="text-light-3 small-medium md:base-regular mt-2">To use Snapgram, Please enter your details</p>
 
 
 
@@ -116,7 +127,7 @@ const SignUpForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isCreatingUser ? (
+            {isCreatingAccount ? (
               <div className="flex-center gap-2 ">
                 <Loader /> Loading...
               </div>
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -69,7 +69,7 @@ const AuthProvider = ({ children} :{children: React.ReactNode }) => {
 
   useEffect(() => {
     if(
-      localStorage.getItem('cookieFallback') === '[]' ||
+      localStorage.getItem('cookieFallback') === '[]' ||  
       localStorage.getItem('cookieFallback') === null
     ) navigate('/sign-in')
 
